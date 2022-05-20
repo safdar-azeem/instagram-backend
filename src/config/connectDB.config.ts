95566c1d3067dde931b4a856e04b9d429aad822c
safdar-azeem <safdarazeem00@gmail.com>
2022-05-20 11:55:00 +0000

    Add connectDB.config.ts

---
+++ src/config/connectDB.config.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const connectToMongoDB = async (): Promise<void> => {
+  const db: string | undefined = process.env.MONGODB_URI
+
+  try {
+    mongoose.set('strictQuery', false)
+    if (db) {
+      await mongoose.connect(db, {
+        autoIndex: true,
+      })
+      console.log('MongoDB connected....')
+    } else {
+      throw new Error('MongoDB URI not found.')
+    }
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  }
+}
+
+export default connectToMongoDB
