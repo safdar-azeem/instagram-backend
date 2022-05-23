85a745a0fb8794e792fa777b4029580a1fc02d0d
safdar-azeem <safdarazeem00@gmail.com>
2022-05-23 16:00:00 +0000

    Add comment.model.ts

---
+++ src/models/comment.model.ts
@@ -0,0 +1,38 @@
+import { Model, model, Schema } from 'mongoose'
+import { CommentDocument } from 'mongoose.gen'
+
+const CommentSchema: Schema<CommentDocument> = new Schema<CommentDocument>({
+   content: {
+      type: String,
+      trim: true,
+   },
+   user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User is required'],
+   },
+   post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+      required: [true, 'Post id is required'],
+   },
+   postUser: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Post user id is required'],
+   },
+   createdAt: {
+      type: Date,
+      default: Date.now,
+   },
+   likes: [
+      {
+         type: Schema.Types.ObjectId,
+         ref: 'User',
+      },
+   ],
+})
+
+const CommentModel: Model<CommentDocument> = model('Comment', CommentSchema)
+
+export default CommentModel
