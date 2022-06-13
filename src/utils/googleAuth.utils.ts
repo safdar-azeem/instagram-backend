168eb0d0c26c85eacb65e82b4dfa9cbe75e1d9e2
safdar-azeem <safdarazeem00@gmail.com>
2022-06-13 22:00:00 +0000

    Update googleAuth.utils.ts

--- src/utils/googleAuth.utils.ts
+++ src/utils/googleAuth.utils.ts
@@ -2,8 +2,8 @@ import { OAuth2Client } from 'google-auth-library'
 
 const client = new OAuth2Client({
    clientId: process.env.GOOGLE_CLIENT_ID,
-   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
    redirectUri: process.env.GOOGLE_REDIRECT_URI,
+   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 })
 
 async function verifyGoogleCredentials(code: string) {
