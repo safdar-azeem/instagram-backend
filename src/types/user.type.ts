c5c4c0215032347ad456882320eb7481f0e3f6f1
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 02:20:05 +0500

    Add user type

---
+++ src/types/user.type.ts
@@ -0,0 +1,37 @@
+const UserTypes = `#graphql
+type AuthData {
+   token: String!
+}
+
+type RegisterResponse {
+   message: String!
+}
+
+input RegisterInput {
+   name: String!
+   email: String!
+   password: String!
+}
+
+type User {
+   _id: ID!
+   name: String!
+   avatar: String
+   location: String
+   website: String
+   cover: String
+   bio: String
+   dateOfBirth: Date
+   createdAt: Date
+   followers: [ID]
+   following: [ID]
+   posts: [ID]
+   totalNotifications: Int
+   totalMessages: Int
+   isVerified: Boolean
+   profession: String
+   bookmarks: [ID]
+}
+`
+
+export default UserTypes
