889e013baf7e6cd9533675b2583842c3b2c0d601
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 02:19:55 +0500

    Add post type

---
+++ src/types/post.type.ts
@@ -0,0 +1,13 @@
+const PostTypes = `#graphql
+type Post {
+   _id: ID!
+   content: String!
+   photo: String
+   user: User!
+   createdAt: Date!
+   likes: [ID]!
+   comments: [ID]!
+   isPublic: Boolean!
+}
+`
+export default PostTypes
