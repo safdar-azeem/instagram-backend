1e024996aa6a4ee102850314813bdeb860ce8074
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 02:20:28 +0500

    Add Post schema

---
+++ src/schema/post.schema.ts
@@ -0,0 +1,22 @@
+import PostTypes from '../types/post.type'
+
+const postSchema = `#graphql
+${PostTypes}
+
+type Query {
+   getAllPosts: [Post]!
+   getPostById(postId: ID!): Post!
+   getPostsByUser(userId: ID!): [Post]!
+   getBookmarkedPosts: [Post]!
+}
+
+type Mutation {
+   uploadPhoto(file: Upload!): String!
+   createPost(content: String, photo: String, isPublic: Boolean): Post!
+   deletePost(postId: ID!): Boolean!
+   likePost(postId: ID!): Boolean!
+   bookmarkPost(postId: ID!): Boolean!
+}
+`
+
+export default postSchema
