30382195b6adc8b5e657660c81e2332a1161c50a
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 02:27:02 +0500

    Add @apollo/server setup

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+require('dotenv').config()
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import { graphqlUploadExpress } from 'graphql-upload-minimal'
+import http from 'http'
+import connectToMongoDB from './config/connectDB.config'
+import authMiddleware from './middleware/auth.middleware'
+import resolvers from './resolvers'
+import typeDefs from './schema'
+
+const app = express()
+const httpServer = http.createServer(app)
+
+const server = new ApolloServer({
+   typeDefs,
+   resolvers,
+   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+   csrfPrevention: false,
+})
+
+const startApolloServer = async () => {
+   try {
+      connectToMongoDB()
+      await server.start()
+      app.use(
+         '/graphql',
+         graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }),
+         cors<cors.CorsRequest>({ origin: '*' }),
+         bodyParser.json(),
+         bodyParser.urlencoded({ extended: true }),
+         expressMiddleware(server, {
+            context: async (context) => {
+               const { user, error } = await authMiddleware(context)
+               return { user, error }
+            },
+         })
+      )
+      await new Promise((resolve: any) => httpServer.listen({ port: 4008 }, resolve))
+      console.log(`🚀  Server ready at: http://localhost:4008/graphql`)
+   } catch (err) {
+      console.error(err.message)
+      process.exit(1)
+   }
+}
+
+startApolloServer()
