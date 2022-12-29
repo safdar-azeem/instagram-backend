cfd1159a306690784c9ea085448f0544f24197b0
safdar-azeem <safdarazeem00@gmail.com>
2022-12-30 02:20:37 +0500

    Add user schema

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,29 @@
+import UserTypes from '../types/user.type'
+
+const UserSchema = `#graphql
+${UserTypes}
+
+type Query {
+   me: User
+   getUserById(id: ID!): User
+}
+
+type Mutation {
+   register(name: String!, email: String!, password: String!): RegisterResponse
+   login(email: String!, password: String!): AuthData
+   updateProfile(
+      name: String
+      bio: String
+      avatar: String
+      location: String
+      website: String
+      cover: String
+      profession: String
+      dateOfBirth: String
+   ): Boolean
+   followOrUnfollowUser(userId: ID!): Boolean
+   searchUsersByName(name: String!): [User]
+}
+`
+
+export default UserSchema
