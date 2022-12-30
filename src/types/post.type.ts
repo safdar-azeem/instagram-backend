884191da13aeef15455d4d8aa75e97a12fd22e5f
safdar-azeem <safdarazeem00@gmail.com>
2022-12-31 03:32:25 +0500

    Update post type

--- src/types/post.type.ts
+++ src/types/post.type.ts
@@ -1,13 +1,13 @@
 const PostTypes = `#graphql
 type Post {
    _id: ID!
-   content: String!
+   content: String
    photo: String
-   user: User!
-   createdAt: Date!
-   likes: [ID]!
-   comments: [ID]!
-   isPublic: Boolean!
+   user: User
+   createdAt: Date
+   likes: [ID]
+   comments: [ID]
+   isPublic: Boolean
 }
 `
 export default PostTypes
