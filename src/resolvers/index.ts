caaf9b7c079aa3a62eeb66cc0f76af676a8976f1
safdar-azeem <safdarazeem00@gmail.com>
2022-12-31 00:56:53 +0500

    Add CommentMutations, CommentQueries

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,16 +1,19 @@
 import { PostMutations, PostQueries } from './post'
 import { UserMutations, UserQueries } from './user'
 import UploadMutation from './upload'
+import { CommentMutations, CommentQueries } from './comment'
 
 const resolvers = {
    Query: {
       ...UserQueries,
       ...PostQueries,
+      ...CommentQueries,
    },
    Mutation: {
       ...UserMutations,
       ...PostMutations,
       ...UploadMutation,
+      ...CommentMutations,
    },
 }
 
