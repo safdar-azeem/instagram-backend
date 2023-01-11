8b9405c1fa75524636b425921c601113ad8f9c47
safdar-azeem <safdarazeem00@gmail.com>
2023-01-11 18:55:55 +0500

    Add Post interface

--- src/models/post.modal.ts
+++ src/models/post.modal.ts
@@ -1,4 +1,16 @@
 import { Document, model, Schema } from 'mongoose'
+import { User } from './user.model'
+import { Comment } from './comment.model'
+
+export interface Post extends Document {
+   content: string
+   photo: string
+   user: User | string
+   createdAt: Date
+   likes: Array<User | string>
+   comments: Array<Comment | string>
+   isPublic: boolean
+}
 
 const postSchema = new Schema({
    content: {
@@ -37,6 +49,6 @@ const postSchema = new Schema({
    },
 })
 
-const PostModel = model('Post', postSchema)
+const PostModel = model<Post>('Post', postSchema)
 
 export default PostModel
