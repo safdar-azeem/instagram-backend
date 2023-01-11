58c7f72ae6d28e13953425c4278c11f5b23ea45b
safdar-azeem <safdarazeem00@gmail.com>
2023-01-11 18:57:06 +0500

    Update Index.ts

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,19 +1,28 @@
-import { PostMutations, PostQueries } from './post'
-import { UserMutations, UserQueries } from './user'
-import UploadMutation from './upload'
-import { CommentMutations, CommentQueries } from './comment'
+import PostQueries from './post/post.query'
+import PostMutations from './post/post.mutation'
+import UserQueries from './user/user.query'
+import UserMutations from './user/user.mutation'
+import CommentQueries from './comment/comment.query'
+import CommentMutations from './comment/comment.mutation'
+import notificationQueries from './notification/notification.query'
+import UploadMutation from './upload/upload.mutation'
+import notificationSubscription from './notification/notification.subscription'
 
 const resolvers = {
    Query: {
       ...UserQueries,
       ...PostQueries,
       ...CommentQueries,
+      ...notificationQueries,
    },
    Mutation: {
       ...UserMutations,
       ...PostMutations,
       ...UploadMutation,
       ...CommentMutations,
+   },
+   Subscription: {
+      ...notificationSubscription,
    },
 }
 
