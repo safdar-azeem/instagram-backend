0435269dc4b86a0e42c6db79a52912c8d07cefd5
safdar-azeem <safdarazeem00@gmail.com>
2023-01-11 18:54:37 +0500

    exporting Notification Model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 import UserModel from './user.model'
 import PostModel from './post.modal'
 import CommentModel from './comment.model'
+import NotificationModel from './notification.modal'
 
-export { UserModel, PostModel, CommentModel }
+export { UserModel, PostModel, CommentModel, NotificationModel }
