5eccab397bebbaaad89eddca75282e185d299a8c
safdar-azeem <safdarazeem00@gmail.com>
2023-01-14 20:52:30 +0500

    Filtering notifications

--- src/resolvers/notification/notification.subscription.ts
+++ src/resolvers/notification/notification.subscription.ts
@@ -1,10 +1,18 @@
 import { GraphQLError } from 'graphql'
 import { pubsub } from '../../index'
+import { withFilter } from 'graphql-subscriptions'
+const mongodb = require('mongodb')
 
 const notificationSubscription = {
    newNotification: {
-      subscribe: () => {
-         return pubsub.asyncIterator(['NEW_NOTIFICATION'])
+      subscribe: (_, args: any, { user, error }) => {
+         if (error) throw new GraphQLError(error.message)
+         const asyncIterator = () => pubsub.asyncIterator(['NEW_NOTIFICATION'])
+         const filterFn = (payload): any => {
+            const userId = mongodb.ObjectId(user._id)
+            return payload.newNotification.receiver.equals(userId)
+         }
+         return withFilter(asyncIterator, filterFn)()
       },
    },
 }
